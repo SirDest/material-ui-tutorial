012ce294601ac0f00962502859ca563610558cf3
SirDest <akinro.destined@gmail.com>
2024-04-16 08:46:39 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+      <Button variant="text" color="primary">
+        Text
+      </Button>
+      <Button variant="outlined" color="warning">
+        Outlined
+      </Button>
+    </div>
+  );
+}
+
+export default App;
